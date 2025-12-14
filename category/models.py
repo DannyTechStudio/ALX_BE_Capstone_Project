69bb16cb42f1f0318737e17169f461f88e2825f2
Daniel Offei <archimedes301@gmail.com>
2025-12-14 19:18:41 +0000

    Implement logic to ensure no duplicate category name for each user

--- category/models.py
+++ category/models.py
@@ -6,5 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='categories')
     
+    # Prevent duplicate category names for the same user
+    class Meta:
+        unique_together = ['name', 'owner']
+    
     def __str__(self):
-        return self.name
+        return f"Category: {self.name} (User: {self.owner.email})"
