d99287318af64e478ed1794cf2c2b79fc0f51130
Daniel Offei <archimedes301@gmail.com>
2026-01-09 21:29:05 +0000

    Fix: Remove the Django hard coded SECRET_KEY from the settings.py file

--- budgetflow_project/settings.py
+++ budgetflow_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("django-insecure-zd-ct4)u6_rjw6l8o3$^uwqalntd3+ne%!d5b#7v(f5ubi3mpd")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
